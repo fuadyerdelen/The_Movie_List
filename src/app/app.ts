1317200f1a8350a4761adaf2fd19ac76ee261a42
m.fuad yerdelen <m.fuadyerdelen@gmail.com>
2025-06-25 15:37:26 +0200

    new project

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet, RouterLink } from '@angular/router';
+import { Subscription } from 'rxjs';
+
+
+
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RouterLink],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
-export class App {
-  protected title = 'MovieList';
+
+export class App implements OnInit, OnDestroy {
+
+
+  isModalOpen = false;
+  isLoadingModal = false;
+  private movieSelectionSub!: Subscription;
+
+
+  ngOnInit(): void {
+
+  }
+
+
+  ngOnDestroy() {
+    if (this.movieSelectionSub) {
+      this.movieSelectionSub.unsubscribe();
+    }
+  }
+
+
+
+
+
+  public getYear(): number {
+    return new Date().getFullYear();
+  }
 }
