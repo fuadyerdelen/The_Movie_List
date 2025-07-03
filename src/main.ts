c4a30f8b655428c4942d0632db2677b256d7daf8
m.fuad yerdelen <m.fuadyerdelen@gmail.com>
2025-07-04 00:24:08 +0200

    routing configuration

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { App } from './app/app';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { routes } from './app/app.routes';
+import { provideRouter } from '@angular/router';
+
 
 bootstrapApplication(App, {
   providers: [
-    provideHttpClient(withFetch())
+    provideHttpClient(withFetch()),
+    provideRouter(routes)
+
   ]
 }).catch(err => console.error(err));
